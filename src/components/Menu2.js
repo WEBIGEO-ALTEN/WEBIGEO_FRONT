9c5aed96d46c7dcedd6aa16243b7d809982e74c3
Thomas Genest <thomas.genest@synetis.com>
2023-05-13 19:29:24 +0200

    some changes

--- src/components/Menu2.js
+++ src/components/Menu2.js
@@ -3,7 +3,6 @@ import { useNavigate } from "react-router-dom";
 
 import axios from "axios";
 
-import CountryDescription from "./CountryDescription";
 import Header from "./Header";
 
 
@@ -40,12 +39,6 @@ function Menu2() {
     var quesionnaries = quizTypes ? Object.fromEntries([...quizTypes].map(type => [type, quizNames.filter(name => name.split('_')[1].toUpperCase() === type).map(name => name.split('_')[0].toUpperCase())])) : {};
 
 
-    var menu = <div>
-        {quiz.map(q => {
-            return <div key={`quiz-${q.pk}`} onClick={() => quizChoice(q)}>{`${q.name} (${q.nbr_question} questions)`}</div>
-        })}
-    </div>
-
     var contientDict = {
         "all":"monde",
         "europe":"europe",
@@ -64,21 +57,15 @@ function Menu2() {
     }
 
     const handleType = (ev) => {
-        setTypes(Object.entries(typeDict).filter(item => item[1] == ev.target.textContent)[0][0]);
+        setTypes(Object.entries(typeDict).filter(item => item[1] === ev.target.textContent)[0][0]);
     }
-    let rendu = countries.map(pay => {
-        return <CountryDescription iso={pay.pk} name={pay.name} flag={pay.flag.slice(0, -2)} shape={pay.shape} cap={pay.capitale} key={pay.pk} cont={pay.continent} />
-    });
+    
     return <div className="bg-dark d-flex flex-column align-items-center h-100">
         <Header />
         {Object.keys(quesionnaries).map((type, i) => {
-
-
             if (types === "") { return <div className={`${type} w-50 h-25 bg-warning text-dark m-2 d-flex justify-content-center align-items-center`} key={`type-${i}`} onClick={handleType}><h1>{typeDict[type]}</h1></div> }
             else {
-                console.log(types)
                 if ( type === types) {
-                    console.log(types)
                     var names = quesionnaries[types].map((name, j) => {
                         return <div className={`name${name} w-50 h-25 bg-warning text-dark m-2 d-flex justify-content-center align-items-center`} key={`type-${types}-name-${j}`} onClick={() => quizChoice(quiz.filter(q => q.name.toUpperCase() === `${name}_${types}`.toUpperCase())[0])}>{contientDict[name.toLowerCase()].toUpperCase()}</div>
                     })
